79cfef19bc4a4e02fd399253ad69887df0239b12
Aditya Chaturvedi <kalelrjbrownboy@gmail.com>
2025-07-08 22:03:51 +0530

    react-router

---
+++ 07reactRouter/src/components/User/User.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useParams } from "react-router";
+function User()
+{const {userid}=useParams();
+  return (
+    
+    <>
+<div className="bg-gray-600">User:{userid}</div>
+    </>
+  )
+}
+export default User;
